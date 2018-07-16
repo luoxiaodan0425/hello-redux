6074caec60db62a04c2b5238ef6bc3045e94bae9
wangh <wangh147>
2018-07-16 16:17:12 +0800

    第一个版本

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+import { INCREMENT,DECREMENT } from '../constants'
+export const increment=(name)=>{
+    return{
+        type:INCREMENT,
+        name
+    }
+};
+
+export const decrement=()=>{
+    return{
+        type:DECREMENT
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
+import {Provider} from 'react-redux'
+
+const store = createStore(rootReducer);
+
+// store.subscribe(()=>console.log("State updata!",store.getState()));
+// store.dispatch({type:'INCREMENT'}
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+     document.getElementById('root')
+);
+
+
+registerServiceWorker();
