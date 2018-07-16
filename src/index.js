734e4feb2d274551989958fba37d8eace8ed3508
wangh <wangh147>
2018-07-16 18:00:37 +0800

    中间件添加

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux';
+import { createStore,applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 import rootReducer from './reducers';
 import {Provider} from 'react-redux'
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, {}, applyMiddleware(logger));
 
 // store.subscribe(()=>console.log("State updata!",store.getState()));
 // store.dispatch({type:'INCREMENT'}
